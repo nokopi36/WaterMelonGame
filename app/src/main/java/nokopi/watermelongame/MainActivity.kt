5c435110613b1145a398d4c0fa8bfba091a0244a
RyotaHiyama <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2023-10-08 17:59:21 +0900

    update:タップした位置に任意の画像を表示できるように

--- app/src/main/java/nokopi/watermelongame/MainActivity.kt
+++ app/src/main/java/nokopi/watermelongame/MainActivity.kt
@@ -1,30 +1,85 @@
 package nokopi.watermelongame
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import nokopi.watermelongame.ui.theme.WaterMelonGameTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            WaterMelonGameTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        // API 30以上の場合
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.decorView.windowInsetsController?.apply {
+                // systemBars : Status barとNavigation bar両方
+                hide(WindowInsets.Type.systemBars())
+                // hide(WindowInsets.Type.statusBars())
+                // hide(WindowInsets.Type.navigationBars())
+                systemBarsBehavior = BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             }
+            // API 29以下の場合
+        } else {
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        }
+        setContent {
+//            WaterMelonGameTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+////                    Greeting("Android")
+//                    ImageAtPosition()
+//                }
+//            }
+//            Center {
+//                Greeting(name = "Android!!")
+//            }
+            ImageAtPosition()
+//            DragTest()
         }
     }
 }
@@ -35,6 +90,82 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun ImageAtPosition() {
+    var positionState by remember {
+        mutableStateOf(Offset(0f, 0f))
+    }
+
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp
+    val screenHeight = configuration.screenHeightDp
+
+    val density = LocalDensity.current.density // ピクセル密度を取得
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectTapGestures { offset ->
+                    positionState = offset
+                }
+            }
+    ) {
+        // タップした位置にImageViewを表示
+        Image(
+            painter = painterResource(R.drawable.img), // 画像リソースを指定
+            contentDescription = null,
+            modifier = Modifier.offset{ IntOffset(positionState.x.roundToInt(), positionState.y.roundToInt()) }
+        )
+    }
+    Center {
+        Column {
+            Text(text = "${positionState.x.roundToInt()}, ${positionState.y.roundToInt()}")
+            Text(text = "$screenWidth, $screenHeight")
+        }
+    }
+}
+
+
+@Composable
+fun DragTest(){
+    var offsetX by remember { mutableStateOf(0f) }
+    var offsetY by remember { mutableStateOf(0f) }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopStart
+    ) {
+        Text(
+            text = "x:${offsetX}\ny:${offsetY}",
+            modifier = Modifier
+                .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        offsetX += dragAmount.x
+                        offsetY += dragAmount.y
+                    }
+                }
+                .background(Color.Green),
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Composable
+fun Center(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        content()
+    }
 }
 
 @Preview(showBackground = true)
